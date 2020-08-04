e07a8494366f475aaca12c3f2124dd32702469eb
KermitTheFrogerino <35975961+KermitTheFrogerino@users.noreply.github.com>
2020-08-04 18:13:49 +0200

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
-sudo chsh --shell=/bin/bash $USER
 
-sudo pamac install github-desktop-bin visual-studio-code-bin discord google-chrome elementary-icon-theme sound-theme-elementary 
-
-##Shell Theme
-sudo pacman -Syy sassc bc inkscape optipng
-git clone https://github.com/hrdwrrsk/adementary-theme.git
-cd ./adementary-theme/
-./parse-sass.sh
-mkdir ~/.themes
-./install.sh -d ~/.themes
+##Bash
+echo "Do you wish to switch to BASH? [y/n]"
+read change_to_bash_var
+if [[ $change_to_bash_var = y ]]
+then
+	sudo chsh --shell=/bin/bash $USER
+fi
+echo ""
 ##
 
-sudo flatpak install Spotify 
+##Applications
+echo "Do you wish to install all apps? [y/n]"
+read install_app_var
+if [[ $install_app_var = y ]]
+then
+	sudo pamac install github-desktop-bin visual-studio-code-bin discord google-chrome
 
-sudo snap install flutter 
+	sudo flatpak install Spotify 
 
-sudo cp ./dark-google-chrome.desktop /usr/share/applications/
+	sudo snap install flutter --classic
+fi
+echo ""
+##
+
+##Shell Theme
+echo "Do you wish to install the Elementary theme? [y/n]"
+read install_theme_var
+if [[ $install_theme_var = y ]]
+then
+	sudo pamac install elementary-icon-theme sound-theme-elementary
+	sudo pacman -Syy sassc bc inkscape optipng 
+	git clone https://github.com/hrdwrrsk/adementary-theme.git
+	cd ./adementary-theme/
+	./parse-sass.sh
+	mkdir ~/.themes
+	./install.sh -d ~/.themes
+fi
+echo ""
+##
+
+## Chrome Dark Mode
+echo "Do you wish to add Chrome Dark? [y/n]"
+read chrome_dark_var
+if [[ $chrome_dark_var = y ]]
+then
+	sudo cp ./dark-google-chrome.desktop /usr/share/applications/
+fi
+echo ""
+##
 
 #Extension Sync
 #git clone https://github.com/oae/gnome-shell-extensions-sync.git
@@ -24,7 +56,9 @@ sudo cp ./dark-google-chrome.desktop /usr/share/applications/
 #yarn install
 #yarn build
 #ln -s "$PWD/dist" "$HOME/.local/share/gnome-shell/extensions/extensions-sync@elhan.io"
+##
 
+## Extension Credentials
 echo "Do you wish to show Extension Credentials? [y/n]"
 read input_variable
 if [[ $input_variable = y ]]
@@ -32,5 +66,9 @@ then
 	echo "Github Gist ID: ***REMOVED***"
 	echo "Github User Token: ***REMOVED***"
 fi
+echo ""
+##
 
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+echo ""
 echo "Done! 😊"
