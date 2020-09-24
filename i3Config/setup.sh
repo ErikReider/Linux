ff262f139e846bbfec80cc9142e37e262abfae23
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-24 11:10:19 +0200

    Update setup.sh

--- i3Config/setup.sh
+++ i3Config/setup.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+
+if [[ $1 == "-i" ]]; then
+    pamac install \
+    ttf-font-awesome \
+    xidlehook \
+    xss-lock \
+    ttf-material-icons-git \
+    pa-applet \
+    ttf-roboto \
+    picom-ibhagwan-git \
+    polybar \
+    scrot \
+    i3lock \
+    rofi \
+    alttab-git \
+    i3-gnome-flashback \
+    pa-applet \
+    gnome-applets \
+    network-manager-applet \
+    rofi-emoji \
+    xsel \
+    gnome-terminal-transparency
+    
+    # To override i3-gnome-flashbacks i3
+    pamac install i3-gaps
+    
+    libtool --finish /usr/lib/rofi/
+fi
+
 currentDir=$PWD
 cd ~/.config/
 dirs=("i3" "picom" "polybar" "rofi")
@@ -7,26 +36,3 @@ for item in ${dirs[@]}; do
     rm ./$item
     ln -s $currentDir/$item/ ./$item
 done
-
-pamac install --no-confirm \
-ttf-font-awesome \
-xidlehook \
-xss-lock \
-ttf-material-icons-git \
-pa-applet \
-ttf-roboto \
-picom-ibhagwan-git \
-polybar \
-scrot \
-i3lock \
-rofi \
-alttab-git \
-i3-gnome-flashback \
-pa-applet \
-gnome-applets \
-network-manager-applet \
-rofi-emoji \
-xsel
-
-# To override i3-gnome-flashbacks i3
-pamac install i3-gaps
