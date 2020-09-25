356f1c43253080ccca0cdc6bab40338ed35f512e
Erik Reider <erik.reider@elev.ga.ntig.se>
2020-09-25 09:01:08 +0200

    font size fixes

--- i3Config/polybar/scripts/bluetooth.sh
+++ i3Config/polybar/scripts/bluetooth.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-    echo "%{F#66ffffff}"
+    echo "%{F#66ffffff}%{T9}%{T-}"
 else
     if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
     then
         echo "%{T9}%{T-}"
     fi
-    echo "%{F#2193ff}"
+    echo "%{F#2193ff}%{T9}%{T-}"
 fi
