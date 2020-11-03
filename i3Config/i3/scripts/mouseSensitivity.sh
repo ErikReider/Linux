fc2d50adaad5391e632cf5acdde9a46dc01996bf
Erik Reider <ereider20010103@gmail.com>
2020-11-03 08:54:34 +0100

    fixed mouse touchpad sensitivity

--- i3Config/i3/scripts/mouseSensitivity.sh
+++ i3Config/i3/scripts/mouseSensitivity.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do
-    if [[ "$id" == *"Touchpad"* ]]; then
-        xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1
+    xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1
+    if [[ "$(xinput list-props $id | grep Device)" != *"Touchpad"* ]]; then
         xinput --set-prop $id 'libinput Accel Speed' -0.2
     fi
 done
