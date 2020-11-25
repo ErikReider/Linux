a8cff59574d6c6a38ed89a7ae540968c3b133dc4
Erik Reider <ereider20010103@gmail.com>
2020-11-25 10:52:15 +0100

    Lock script

--- i3Config/i3/scripts/lock.sh
+++ i3Config/i3/scripts/lock.sh
@@ -4,5 +4,7 @@ rm /tmp/.i3_lock.png
 tmpfile=/tmp/.i3_lock.png
 
 scrot "$tmpfile"
-convert "$tmpfile" -blur 0x20 "$tmpfile"
-i3lock -i "$tmpfile" -t --ignore-empty-password --show-failed-attempts --nofork -p default; sleep 1
+convert "$tmpfile" -scale 2.5% -resize 4000% "$tmpfile"
+i3lock -i "$tmpfile" -t --ignore-empty-password --show-failed-attempts --nofork -p default
+sleep 10 * 60
+xset dpms force off
