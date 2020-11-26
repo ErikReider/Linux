10878fc31f0d412e891887cc061d83ddbd859319
Erik Reider <ereider20010103@gmail.com>
2020-11-26 13:58:25 +0100

    Fixed lockscreen shutting off the screen on unlock

--- i3Config/i3/scripts/lock.sh
+++ i3Config/i3/scripts/lock.sh
@@ -6,5 +6,3 @@ tmpfile=/tmp/.i3_lock.png
 scrot "$tmpfile"
 convert "$tmpfile" -scale 2.5% -resize 4000% "$tmpfile"
 i3lock -i "$tmpfile" -t --ignore-empty-password --show-failed-attempts --nofork -p default
-sleep 10 * 60
-xset dpms force off
