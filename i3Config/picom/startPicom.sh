5ea387d98b6c066f62c8e02477314372fe11065b
Erik Reider <ereider20010103@gmail.com>
2020-12-04 15:37:54 +0100

    Fixed picom not always starting when reloading i3

--- i3Config/picom/startPicom.sh
+++ i3Config/picom/startPicom.sh
@@ -3,4 +3,7 @@ if ps -A | grep picom; then
     killall -q picom
 fi
 
+# # Wait until the processes have been shut down
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+
 picom --config $HOME/.config/picom/picom.conf --experimental-backends --xrender-sync-fence --log-file $HOME/picom.log
