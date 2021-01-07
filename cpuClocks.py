1363f1f3d6b3276b0cad5493369b3d3ca7c25163
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-01-07 21:36:51 +0100

    Shell script for checking available colors and added cpu clock script

---
+++ cpuClocks.py
@@ -0,0 +1,23 @@
+import subprocess
+import time
+from typing import List
+import math
+
+maxClocks: List[int] = []
+print("\x1bc" + "/proc/cpuinfo:")
+while True:
+    all_info = str(subprocess.check_output("cat /proc/cpuinfo", shell=True).strip().decode()).splitlines()
+    lines: List[str] = []
+    for line in all_info:
+        if "cpu MHz" in line:
+            clock = math.floor(float(line.replace("\t", "").split(": ")[1]))
+            index = len(lines)
+            if len(maxClocks) <= index:
+                maxClocks.append(clock)
+            else:
+                if maxClocks[index - 1] < clock:
+                    maxClocks[index - 1] = clock
+            maxClk = str(maxClocks[index - 1])
+            lines.append("CPU {0}:\t {1}MHz".format(index, clock) + "\tMaxClock: {0}MHz".format(maxClk))
+    print(*lines, sep="\n", end=("\x1b[1A" * (len(lines) - 1)) + "\r", flush=True)
+    time.sleep(0.4)
