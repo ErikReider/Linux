bec8239b42034ae56a94cdc308021be0f1df9b32
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-01-11 18:10:00 +0100

    Added ability to ignore sleep

--- i3Config/i3/scripts/mainLockScript.sh
+++ i3Config/i3/scripts/mainLockScript.sh
@@ -1,3 +1,4 @@
+source ~/.config/i3/settings
 # Disable defaults
 # https://wiki.archlinux.org/index.php/Display_Power_Management_Signaling
 # https://www.linuxquestions.org/questions/arch-29/screen-goes-blank-after-ten-minutes-in-lxde-how-to-change-this-769000/
@@ -11,14 +12,14 @@ while pgrep -u $UID -x xidlehook >/dev/null; do sleep 1; done
 # xss-lock grabs a logind suspend inhibit lock and will use i3lock to lock the
 # screen before suspend. Use loginctl lock-session to lock your screen.
 xss-lock --transfer-sleep-lock -- ~/.config/i3/scripts/lock.sh &
-# Activate the idle listener with it's callbacks
-xidlehook --not-when-fullscreen --not-when-audio \
-  --timer 900 \
-    './dimAllDisplays.sh -d' \
-    './dimAllDisplays.sh' \
-  --timer 10 \
-    'loginctl lock-session; ./dimAllDisplays.sh' \
-    '' \
-  --timer 30 \
-    'xset dpms force off' \
-    '' &
+
+if [ "$shouldSleep" = true ]; then
+    # Activate the idle listener with it's callbacks
+    xidlehook --not-when-fullscreen --not-when-audio \
+      --timer $lockTimer \
+        'loginctl lock-session; ./dimAllDisplays.sh' \
+        '' \
+      --timer $sleepTimer \
+        'xset dpms force off' \
+        '' &
+fi
