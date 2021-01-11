4f681bf60b2e24a553bbb82eb1828d602761a2c4
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-01-11 18:10:44 +0100

    Added ability to whitelist some touchpads that don't include 'Touchpad' in the device name

--- i3Config/i3/scripts/mouseSensitivity.sh
+++ i3Config/i3/scripts/mouseSensitivity.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+IFS=$'\n'
+
+function setMouseSettings() {
+    xinput --set-prop $id 'libinput Accel Speed' -0.2
+}
+
+function setTouchPadSettings() {
+    xinput --set-prop $id 'libinput Accel Speed' 1
+    xinput --set-prop $id 'libinput Natural Scrolling Enabled' 1
+}
+
+whitelist=`cat ~/.config/i3/touchpadWhitelist.txt`
 
 for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do
+    name=$(xinput list-props $id | grep "Device '" | awk -F"'" '$0=$2')
     xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1
-    if [[ "$(xinput list-props $id | grep Device)" != *"Touchpad"* ]]; then
-        xinput --set-prop $id 'libinput Accel Speed' -0.2
+    if [[ "${whitelist[@]}" =~ "${name}" ]] || [[ "$name" == *"Touchpad"* ]]; then
+        # in Whitelist or name is Touchpad
+        setTouchPadSettings
     else
-        xinput --set-prop $id 'libinput Accel Speed' 1
-        xinput --set-prop $id 'libinput Natural Scrolling Enabled' 1
+        # Other pointers
+        setMouseSettings
     fi
 done
