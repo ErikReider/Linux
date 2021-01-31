bedd3bff0b36b13b9e23a2f89e5f87be70222a78
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-01-31 22:25:20 +0100

    Now able to search

--- findSteamGame.sh
+++ findSteamGame.sh
@@ -1,1 +1,26 @@
-( IFS=$'\n'; for D in ~/.steam/root $(egrep '^[^"]*"[0-9]+"' ~/.steam/root/steamapps/libraryfolders.vdf | cut -d\" -f4); do for F in "$D"/steamapps/appmanifest_*.acf; do printf "%10s %s\n" $(grep '"appid"' "$F" | cut -d\" -f4) $(grep '"name"' "$F" | cut -d\" -f4); done; done ) | sort -g
+#/bin/bash
+
+IFS=$'\n'
+
+games=()
+found=false
+for D in ~/.steam/root $(egrep '^[^"]*"[0-9]+"' ~/.steam/root/steamapps/libraryfolders.vdf | cut -d\" -f4); do 
+    for F in "$D"/steamapps/appmanifest_*.acf; do 
+        name="$(grep '"name"' "$F" | cut -d\" -f4)"
+        id="$(grep '"appid"' "$F" | cut -d\" -f4)"
+        if [[ "$1" != "" ]] && [[ ${name^} == *"${1^}"* ]]; then
+            echo $id $name
+            found=true
+            continue
+        fi
+        games+=("$id $name")
+    done
+done
+
+if [[ $found == true ]]; then
+    exit 0
+fi
+
+games=($(sort -g <<<"${games[*]}"))
+
+for game in ${games[@]}; do echo $game; done
