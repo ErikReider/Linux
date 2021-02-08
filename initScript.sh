9d019644b030f4c7c46423bbbf70df5c12ccf4f2
Erik Reider <ereider20010103@gmail.com>
2021-02-08 15:40:55 +0100

    Remove ulauncher and chrome dark, added digestif

--- initScript.sh
+++ initScript.sh
@@ -39,9 +39,9 @@ then
     pamac install github-desktop-bin visual-studio-code-bin google-chrome android-studio nautilus-copy-path android-messages-desktop-bin dotnet-sdk neovim jq dart
 
     sudo pacman -Syy --needed discord auto-cpufreq jdk-openjdk jdk11-openjdk jdk8-openjdk jre-openjdk jre-openjdk-headless jre11-openjdk jre11-openjdk-headless jre8-openjdk jre8-openjdk-headless nodejs npm bash-completion
-    
+
     sudo flatpak install Spotify
-    
+
     sudo snap install flutter --classic
 
     pip install jedi
@@ -59,28 +59,22 @@ echo ""
 ## Neovim
 read -p "Do you wish to link vim config files? [y/n] " vim_var
 if [[ $vim_var = y ]]; then
-    pamac install ccls
+    pamac install ccls texlive-core
     pip install cpplint
+
+    # Install digestif for latex
+    wget -P ~/.local/bin https://raw.githubusercontent.com/astoff/digestif/master/scripts/digestifi
+    chmod +x ~/.local/bin/digestif
 
     currentDir=$PWD
     cd ~/.config/
-    rm -rf nvim 
+    rm -rf nvim
 
     ln -s $currentDir/nvim nvim
-    cd $currentDir/.. 
+    cd $currentDir/..
 
-    sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' 
+    sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
-fi
-echo ""
-##
-
-## Ulauncher
-read -p "Do you wish to install Ulauncher? [y/n] " install_ulauncher_var
-if [[ $install_ulauncher_var = y ]]
-then
-    pamac install ulauncher
-    sed -i 's/<Primary>space/<alt>space/g' ~/.config/ulauncher/settings.json
 fi
 echo ""
 ##
@@ -98,19 +92,6 @@ then
     ./install.sh -d ~/.themes
     cd ..
     rm -rf ./adementary-theme
-fi
-echo ""
-##
-
-## Chrome Dark Mode
-read -p  "Do you wish to add Chrome Dark? [y/n] " chrome_dark_var
-if [[ $chrome_dark_var = y ]]
-then
-    #sudo cp ./google-chrome.desktop /usr/share/applications/
-    # find1="google-chrome-stable"
-    # replace1="google-chrome-stable --enable-features=WebUIDarkMode --force-dark-mode"
-    # sudo sed -i "s|$find1|$replace1|g" /usr/share/applications/google-chrome.desktop
-    cp ./assets/chromium-flags.conf ~/.config/
 fi
 echo ""
 ##
