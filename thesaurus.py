178dcff73cbe60c5990d38fb6c45354a347fb55f
Erik Reider <ereider20010103@gmail.com>
2021-02-11 10:37:15 +0100

    Fixed error when no words found

--- thesaurus.py
+++ thesaurus.py
@@ -9,8 +9,11 @@ parser.add_argument('strings', metavar='N', type=str, help='The word to search f
 
 url = "https://tuna.thesaurus.com/relatedWords/{0}?limit=1".format(parser.parse_args().strings)
 req = requests.get(url)
-data = json.loads(req.text)["data"][0]["synonyms"]
+data = json.loads(req.text)
+if data["data"] == None:
+    print("No words found...")
+    exit(0)
 
-data = list(map(lambda x: x["term"], data))
+data = list(map(lambda x: x["term"], data["data"][0]["synonyms"]))
 
 [print(x) for x in data]
