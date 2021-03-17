ee97794705acd75d0ecd06d822798f8b022a4882
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-03-17 21:58:03 +0100

    Moved Sway config to seperate files

--- dotfiles/swayConfig/setup.sh
+++ dotfiles/swayConfig/setup.sh
@@ -4,62 +4,47 @@ if [[ $1 == "-i" ]]; then
     pamac install --no-upgrade \
         wl-clipboard \
         wdisplays \
-        wl-recorder \
+        wf-recorder \
         xdg-desktop-portal-wlr \
         grim \
         slurp \
         swayidle \
+        wallutils \
         wallutils \
         kanshi \
         grim \
         slurp \
         swappy \
         otf-font-awesome \
-        i3-gaps \
         ttf-font-awesome \
-        xss-lock \
         pa-applet \
         ttf-roboto \
-        polybar \
-        scrot \
-        i3lock \
         brightnessctl \
-        pa-applet \
         gnome-applets \
         network-manager-applet \
-        xsel \
         blueberry \
         bluez-utils \
         playerctl \
-        redshift \
         appimagelauncher \
         libappimage \
-        nitrogen \
         lxappearance \
-        arandr \
-        autorandr \
         kitty \
-        clipit
+        pacdep \
+        sway
 
     pamac install pod2man
 
     # AUR
     pamac install --no-upgrade \
+        waybar-git \
         dunst-git \
         swaylock-effects \
         rofi-lbonn-wayland-git \
         wlsunset \
-        autotiling-git \
         i3lock-fancy-rapid-git \
         polkit-gnome-gtk2 \
-        alttab-git \
-        picom-git\
         rofi-emoji \
-        gnome-terminal-transparency \
         ttf-material-icons-git \
-        xidlehook \
-        xob \
-        touchegg \
         ttf-weather-icons
     
     libtool --finish /usr/lib/rofi/
