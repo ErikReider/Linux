469f13c04dc58eac07949f62adee8412f0be638b
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-03-18 23:03:19 +0100

    Add bash-language-server, fixed sway-screenshare.sh

--- initScript.sh
+++ initScript.sh
@@ -75,7 +75,7 @@ echo ""
 read -p "Do you wish to link vim config files? [y/n] " vim_var
 if [[ $vim_var = y ]]; then
     pamac install ccls texlive-bibtexextra texlive-gantt texlive-pictures
-    sudo pacman -Syu texlive-core texlive-fontsextra texlive-latexextra texlive-science biber
+    sudo pacman -Syu texlive-core texlive-fontsextra texlive-latexextra texlive-science biber bash-language-server
     pip install cpplint
 
     # Install digestif for latex
--- sway-screenshare.sh
+++ sway-screenshare.sh
@@ -25,11 +25,12 @@ function geometry {
 # Ensure we're not using the wayland backend on SDL
 unset SDL_VIDEODRIVER
 
-geometry=$(geometry) || exit $?
+# geometry=$(geometry) || exit $?
 # wf-recorder -c rawvideo --geometry="$geometry" -m sdl -f pipe:wayland-mirror
 # wf-recorder --geometry="$geometry" -m v4l2 -c rawvideo -f /dev/video$videoNr -x yuv420p
 
 
 # Alternative method via ffplay
 # wf-recorder -c rawvideo --geometry="$geometry" -x yuv420p -m avi -f pipe:99 99>&1 >&2 | ffplay -f avi - &
-wf-recorder -c rawvideo --geometry="$geometry" -x yuv420p -m v4l2 -f /dev/video$videoNr
+# wf-recorder --muxer=v4l2 --codec=rawvideo --geometry="$geometry" -x yuv420p  -f /dev/video$videoNr
+wf-recorder --muxer=v4l2 --codec=rawvideo --pixel-format=yuv420p --file=/dev/video$videoNr
