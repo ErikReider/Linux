d0af397c68a20a4179b0d8dc1e4ffedf35e50aaa
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-04-18 22:12:58 +0200

    Changed wallpaper loc in random wall script

--- dotfiles/swayConfig/sway/scripts/randomWallpaper.sh
+++ dotfiles/swayConfig/sway/scripts/randomWallpaper.sh
@@ -8,16 +8,16 @@ if [ -z "$SWAYSOCK" ]; then
   export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
 fi
 
-wlpath=${WALLPAPER_PATH:-"$HOME/.config/wallpaper.jpg"}
+wlpath=${WALLPAPER_PATH:-"$HOME/.cache/wallpaper"}
 output=${WALLPAPER_OUTPUT:-"*"}
 url="https://source.unsplash.com/1920x1080/?city,night,ocean,space,fire,hell"
 
 # Checks if gotten wallpaper within 1 hour
-if [[ -f $HOME/.config/wallpaper.jpg ]]; then
+if [[ -f $HOME/.cache/wallpaper ]]; then
     time=$(date +%s)
-    create=$(stat -c %Y $HOME/.config/wallpaper.jpg)
+    create=$(stat -c %Y $HOME/.cache/wallpaper)
 fi
-if [[ ! -f $HOME/.config/wallpaper.jpg ]] || [[ "$1" == "--click" ]] || [[ $((($time - $create) / 60)) -ge 60 ]]; then
+if [[ ! -f $HOME/.cache/wallpaper ]] || [[ "$1" == "--click" ]] || [[ $((($time - $create) / 60)) -ge 60 ]]; then
     curl -L "$url" -s > $wlpath
 fi
 
