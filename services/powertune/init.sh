5fd6ee821da9ccbfaaa30efda93e4c5b29171088
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-05-25 16:00:50 +0200

    Removed unused service and wallpaperHelper folder

--- services/powertune/init.sh
+++
@@ -1,4 +0,0 @@
-sudo cp ./powertopAutoTune.service /etc/systemd/system/
-sudo cp ./powertune.sh /usr/bin/
-sudo systemctl enable powertopAutoTune.service
-sudo systemctl start powertopAutoTune.service
