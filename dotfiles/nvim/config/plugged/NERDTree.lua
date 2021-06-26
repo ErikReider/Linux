148f5ff4d7e7be479bdcb6053ad30a1b38d71641
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-06-27 01:35:35 +0200

    Converted NERDTree config to lua

---
+++ dotfiles/nvim/config/plugged/NERDTree.lua
@@ -0,0 +1,16 @@
+-- Navigate to CWD on nerdtree open
+function _G.openNerdTree()
+    local command = ""
+    if vim.g.NERDTree and vim.api.nvim_eval("g:NERDTree.IsOpen()") == 1 then
+        command = ":NERDTreeClose"
+    else
+        command = ":NERDTreeCWD"
+    end
+    return vim.api.nvim_replace_termcodes(command .. "<CR>", true, true, true)
+end
+vim.api.nvim_set_keymap('n', '<C-n>', 'v:lua.openNerdTree()', {expr = true})
+vim.api.nvim_set_keymap('n', '<C-m>', ":NERDTreeFind <CR>", {})
+
+vim.api.nvim_set_var("NERDTreeGitStatusWithFlags", 1)
+vim.api.nvim_set_var("WebDevIconsUnicodeDecorateFolderNodes", 1)
+vim.api.nvim_set_var("NERDTreeIgnore", {'^node_modules$'})
