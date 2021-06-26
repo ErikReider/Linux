d8b10175c964926abc6db7663ea4fc8e8f4333df
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-06-26 20:21:38 +0200

    Added scdoc to initScript

--- initScript.sh
+++ initScript.sh
@@ -69,6 +69,7 @@ echo ""
 read -p "Do you wish to link vim config files? [y/n] " vim_var
 if [[ $vim_var = y ]]; then
     yay -S --needed neovim-nightly-bin ccls texlive-bibtexextra texlive-gantt texlive-pictures vala-language-server-git the_silver_searcher bat ripgrep efm-langserver vint lua-format ueberzug
+    yay -S --needed scdoc
     sudo pacman -S --needed texlive-core texlive-fontsextra texlive-latexextra texlive-science biber bash-language-server uncrustify shfmt python2
     pip install cpplint neovim
     sudo npm install -g neovim
