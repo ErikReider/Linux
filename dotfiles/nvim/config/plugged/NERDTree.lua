53a599988c701303de006323299e2fa0bd7964d1
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-06-27 14:41:50 +0200

    NERDTree open, close now silent

--- dotfiles/nvim/config/plugged/NERDTree.lua
+++ dotfiles/nvim/config/plugged/NERDTree.lua
@@ -8,8 +8,8 @@ function _G.openNerdTree()
     end
     return vim.api.nvim_replace_termcodes(command .. "<CR>", true, true, true)
 end
-vim.api.nvim_set_keymap('n', '<C-n>', 'v:lua.openNerdTree()', {expr = true})
-vim.api.nvim_set_keymap('n', '<C-m>', ":NERDTreeFind <CR>", {})
+vim.api.nvim_set_keymap('n', '<C-n>', 'v:lua.openNerdTree()', {silent = true, expr = true})
+vim.api.nvim_set_keymap('n', '<C-m>', ":NERDTreeFind <CR>", {silent = true})
 
 vim.api.nvim_set_var("NERDTreeGitStatusWithFlags", 1)
 vim.api.nvim_set_var("WebDevIconsUnicodeDecorateFolderNodes", 1)
