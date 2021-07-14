20b1e9a35bd10b16b9882d4847ee1c43e5531e75
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-07-14 16:12:51 +0200

    NVIM: Added more git commands to floating window

--- dotfiles/nvim/lua/custom/gitWindow.lua
+++ dotfiles/nvim/lua/custom/gitWindow.lua
@@ -1,7 +1,9 @@
 -- Open git options
 local gitWindowOptions = vim.inspect({
     {title = "LazyGit", action = "LazyGit"},
-    {title = "Git Diff", action = "Gdiffsplit"}
+    {title = "Git Diff", action = "Gdiffsplit"},
+    {title = "Git log", action = "GV"},
+    {title = "Open in browser", action = "GBrowse"},
 }, {newline = '', indent = ""})
 map("n", "<Leader>g",
     ":lua require('floatingWindow').open(" .. gitWindowOptions .. ")<CR>",
