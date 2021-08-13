517ca7fb7855ea330b4c3914a315f8e0bf2426bd
Erik Reider <35975961+erikreider@users.noreply.github.com>
2021-08-13 19:25:18 +0200

    Waybar: Added new dynamic distro icon script

---
+++ dotfiles/swayConfig/waybar/scripts/systemIcon.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+case $(lsb_release -si) in
+"ManjaroLinux")
+    echo ""
+    ;;
+*"Arch"*)
+    echo ""
+    ;;
+*"Debian"*)
+    echo ""
+    ;;
+*"Alpine"*)
+    echo ""
+    ;;
+*"CentOS"*)
+    echo ""
+    ;;
+*"Elementary"*)
+    echo ""
+    ;;
+*"Fedora"*)
+    echo ""
+    ;;
+*"Gentoo"*)
+    echo ""
+    ;;
+"Linux Mint"* | "LinuxMint"* | "mint"*)
+    echo ""
+    ;;
+*"Raspian"*)
+    echo ""
+    ;;
+*"Redhat"*)
+    echo ""
+    ;;
+*"Ubuntu"*)
+    echo ""
+    ;;
+*)
+    echo ""
+    ;;
+esac
