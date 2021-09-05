ea8406755dcfd9fe3c21654629b9a4d5a623bfc2
Erik Reider <ereider20010103@gmail.com>
2021-09-05 22:54:08 +0200

    Sway: Fixed waybar headsetcontrol charge pango markdown not working

--- dotfiles/swayConfig/waybar/scripts/headsetcontrol.sh
+++ dotfiles/swayConfig/waybar/scripts/headsetcontrol.sh
@@ -3,7 +3,7 @@ charge=`headsetcontrol -b 2>&1 | grep "Battery: " | awk '{ print $2 }'`
 if [ "$charge" != "Unavailable" ]; then
     if [[ "$charge" == "" ]]; then charge="100%"; fi
     if [[ "$charge" == "Charging" ]]; then 
-        charge="<span size='small'></span>"
+        charge=""
     fi
     echo "  $charge"
 else
