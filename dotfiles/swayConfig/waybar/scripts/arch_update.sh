55c0b3e82036d20337c111c522af2d34a7427eff
Erik Reider <ereider20010103@gmail.com>
2021-10-21 11:02:34 +0200

    Waybar: Updated update script

--- dotfiles/swayConfig/waybar/scripts/arch_update.sh
+++ dotfiles/swayConfig/waybar/scripts/arch_update.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/zsh
 
-yay -Sy
-yay -Qu
-if updates_arch=$(yay -Qqu 2>/dev/null | wc -l); then
-    yay -Su
+if hash paru 2>/dev/null; then
+    cmd="paru"
+elif hash yay 2>/dev/null; then
+    cmd="yay"
+elif hash pikaur 2>/dev/null; then
+    cmd="yay"
+elif hash pacman 2>/dev/null; then
+    cmd="pacman"
 fi
-flatpak update
-sudo snap refresh
+if [[ $cmd != "" ]]; then
+    $cmd -Syu
+fi
+
+if hash flatpak 2>/dev/null; then
+    flatpak update
+fi
+
+if hash snap 2>/dev/null; then
+    sudo snap refresh
+fi
