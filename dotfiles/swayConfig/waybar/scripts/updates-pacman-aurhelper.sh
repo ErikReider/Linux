f1172139776a42cf18cee97f1735499a1dc4f201
Erik Reider <ereider20010103@gmail.com>
2021-10-21 11:52:12 +0200

    Waybar: Fixed check update script not working

--- dotfiles/swayConfig/waybar/scripts/updates-pacman-aurhelper.sh
+++ dotfiles/swayConfig/waybar/scripts/updates-pacman-aurhelper.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
-
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
+if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
     updates_arch=0
 fi
 
-# if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
-# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
-    # updates_aur=0
-# fi
+if hash paru 2>/dev/null; then
+    cmd="paru"
+elif hash yay 2>/dev/null; then
+    cmd="yay"
+elif hash pikaur 2>/dev/null; then
+    cmd="pikaur"
+elif hash pacman 2>/dev/null; then
+    cmd="pacman"
+fi
+updates_aur=0
+if [[ $cmd != "" ]]; then
+    if ! updates_aur=$($cmd -Qum 2>/dev/null | wc -l); then
+        updates_aur=0
+    fi
+fi
 
-# updates=$(("$updates_arch" + "$updates_aur"))
+updates=$(("$updates_arch" + "$updates_aur"))
 
-# if [ "$updates" -gt 0 ]; then
-    # echo "  # $updates "
-# else
-    # echo "  "
-# fi
-
-if [ "$updates_arch" -gt 0 ]; then
-    echo " #$updates_arch "
+if [ "$updates" -gt 0 ]; then
+    echo " #$updates "
 else
     echo "  "
 fi
-
