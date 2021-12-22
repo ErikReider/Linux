f9607a7be5014eb651e785e1869fc2be988f1118
Erik Reider <ereider20010103@gmail.com>
2021-12-22 10:41:52 +0100

    NVIM: Refactored treesitter config

--- dotfiles/nvim/lua/plugged/treesitter.lua
+++ dotfiles/nvim/lua/plugged/treesitter.lua
@@ -1,11 +1,19 @@
 vim.cmd('set foldmethod=expr')
 vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
-require('nvim-treesitter.configs').setup({
+
+local status_ok, config = pcall(require, "nvim-treesitter.configs")
+if not status_ok then return end
+
+config.setup({
     ensure_installed = "all",
-    highlight = {enable = true},
+    highlight = {
+        enable = true,
+        disable = {""},
+        additional_vim_regex_highlighting = true
+    },
     rainbow = {
         enable = true,
         extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-        max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
+        max_file_lines = nil -- Do not enable for files with more than 1000 lines, int
     }
 })
