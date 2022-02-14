1c56ebc662967fea4a357b22191f599930183f81
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-02-14 11:41:39 +0100

    Sway: Removed lock bloat

--- dotfiles/swayConfig/sway/scripts/lock.sh
+++ dotfiles/swayConfig/sway/scripts/lock.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-if [[ "$1" == "--lock" ]]; then
+if [[ $1 == "--lock" ]]; then
     profilePic=$(~/.config/sway/scripts/generateRoundProfilePic.sh)
-    swaylock daemonize --effect-compose "96x96;$profilePic"
+    swaylock --daemonize --effect-compose "96x96;$profilePic"
 else
     xset s off
     # Killall these prev processes
-    killall -9 xss-lock
     killall -9 swayidle
     # Wait until the processes have been shut down
-    while pgrep -u $UID -x xss-lock >/dev/null; do sleep 1; done
     while pgrep -u $UID -x swayidle >/dev/null; do sleep 1; done
-
-    xss-lock --transfer-sleep-lock -- ~/.config/sway/scripts/lock.sh --lock &
 
     # Locks after 300s, displays off after +300s. Lock screen before sleep
     swayidle -w \
         timeout 600 'loginctl lock-session' \
         timeout 1200 'systemctl suspend' \
-        before-sleep 'loginctl lock-session'
-    # resume 'swaymsg "output * dpms on"' \
+        before-sleep 'loginctl lock-session' \
+        lock '~/.config/sway/scripts/lock.sh --lock' &
 fi
