5cb9c599f7288eb571cca18a83756b150705a330
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-05-19 19:03:54 +0200

    NVIM: MarkdownPreview fixes

--- dotfiles/nvim/lua/plugged/markdown-preview.lua
+++ dotfiles/nvim/lua/plugged/markdown-preview.lua
@@ -1,4 +1,4 @@
-map("n", "<C-M-p>", "<Plug>MarkdownPreviewToggle", {})
+-- map("n", "<C-M-p>", "<Plug>MarkdownPreviewToggle", {}0
 
 -- set to 1, nvim will open the preview window after entering the markdown buffer
 -- default: 0
@@ -7,7 +7,7 @@ vim.g.mkdp_auto_start = 0
 -- set to 1, the nvim will auto close current preview window when change
 -- from markdown buffer to another buffer
 -- default: 1
-vim.g.mkdp_auto_close = 1
+vim.g.mkdp_auto_close = 0
 
 -- set to 1, the vim will refresh markdown when save the buffer or
 -- leave from insert mode, default 0 is auto refresh markdown as you edit or
@@ -18,7 +18,7 @@ vim.g.mkdp_refresh_slow = 0
 -- set to 1, the MarkdownPreview command can be use for all files,
 -- by default it can be use in markdown file
 -- default: 0
-vim.g.mkdp_command_for_global = 0
+vim.g.mkdp_command_for_global = 1
 
 -- set to 1, preview server available to others in your network
 -- by default, the server listens on localhost (127.0.0.1)
