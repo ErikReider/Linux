9a3d274600e033c2647687504d64cd3d427e5162
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-06-22 21:02:16 +0200

    NVIM: Updated dashboard config

--- dotfiles/nvim/lua/plugged/dashboard-nvim.lua
+++ dotfiles/nvim/lua/plugged/dashboard-nvim.lua
@@ -1,68 +1,55 @@
-vim.g.dashboard_custom_header = {
-    "                .88888888:.", "               88888888.88888.",
-    "             .8888888888888888.", "             888888888888888888",
-    "             88' _`88'_  `88888", "             88 88 88 88  88888",
-    "             88_88_::_88_:88888", "             88:::,::,:::::8888",
-    "             88`:::::::::'`8888", "            .88  `::::'    8:88.",
-    "           8888            `8:888.",
-    "         .8888'             `888888.",
-    "        .8888:..  .::.  ...:'8888888:.",
-    "       .8888.'     :'     `'::`88:88888",
-    "      .8888        '         `.888:8888.",
-    "     888:8         .           888:88888",
-    "   .888:88        .:           888:88888:",
-    "   8888888.       ::           88:888888",
-    "   `.::.888.      ::          .88888888",
-    "  .::::::.888.    ::         :::`8888'.:.",
-    " ::::::::::.888   '         .::::::::::::",
-    " ::::::::::::.8    '      .:8::::::::::::.",
-    ".::::::::::::::.        .:888:::::::::::::",
-    ":::::::::::::::88:.__..:88888:::::::::::'",
-    " `'.:::::::::::88888888888.88:::::::::'",
-    "       `':::_:' -- '' -'-' `':_::::'`"
+local db = require("dashboard")
+db.hide_statusline = true
+db.hide_tabline = true
+
+db.custom_header = {
+    "                                   ",
+    "         ((##           (((        ",
+    "       ((((###(         (((((      ",
+    "    //*(((((((((        ((((((((   ",
+    "    ////*(((((((((      ((((((((   ",
+    "    ///////((((((((,    ////////   ",
+    "    *******/(((((((((   ////////   ",
+    "    ******** (((((((((( ////////   ",
+    "    ********   (((((((((////////   ",
+    "    ********     ///////////////   ",
+    "    ********      //////////////   ",
+    "    ********        /////////***   ",
+    "       *****         ,///////      ",
+    "         ,,,           ////        ", "", ""
 }
 
-vim.g.dashboard_custom_section = {
-    a = {
-        description = {" New File                  Leader n    "},
-        command = "DashboardNewFile"
-    },
-    b = {
-        description = {" Recent Files              Alt shift h "},
-        command = "History"
-    },
-    c = {
-        description = {" Open Last Session         Leader s l  "},
-        command = "SessionLoad"
-    },
-    d = {
-        description = {" Find File                 Ctrl f      "},
-        command = "Files"
-    },
-    e = {
-        description = {" Find Word                 Alt shift f "},
-        command = "Rg"
+db.custom_center = {
+    {
+        icon = " ",
+        desc = "New File                  ",
+        action = "DashboardNewFile",
+        shortcut = "Leader n    "
+    }, {
+        icon = " ",
+        desc = "Find File                 ",
+        action = "lua telescopeGFiles(true)",
+        shortcut = "Ctrl f      "
+    }, {
+        icon = " ",
+        desc = "Find Word                 ",
+        action = "Telescope live_grep",
+        shortcut = "Alt shift f "
     }
 }
 
-vim.g.dashboard_default_executive = "telescope"
-vim.api.nvim_set_keymap("n", "<Leader>ss", ":<C-u>SessionSave<CR>", {})
-vim.api.nvim_set_keymap("n", "<Leader>sl", ":<C-u>SessionLoad<CR>", {})
-vim.api.nvim_set_keymap("n", "<Leader>n", ":DashboardNewFile<CR>",
-                        {noremap = true, silent = true})
 
-local utils = require('telescope.utils')
-local set_var = vim.api.nvim_set_var
+local utils = require("telescope.utils")
+
+local function get_dashboard_git_status()
+    local git_cmd = {"git", "status", "-s", "--", "."}
+    local output = utils.get_os_command_output(git_cmd)
+    db.custom_footer = {"Git status:", unpack(output)}
+end
 
 local _, ret = utils.get_os_command_output({
     "git", "rev-parse", "--show-toplevel"
 }, vim.loop.cwd())
-
-local function get_dashboard_git_status()
-    local git_cmd = {'git', 'status', '-s', '--', '.'}
-    local output = utils.get_os_command_output(git_cmd)
-    set_var('dashboard_custom_footer', {'Git status', '', unpack(output)})
-end
 
 if ret ~= 0 then
     local is_worktree = utils.get_os_command_output({
@@ -71,7 +58,7 @@ if ret ~= 0 then
     if is_worktree[1] == "true" then
         get_dashboard_git_status()
     else
-        set_var('dashboard_custom_footer', {'Not in a git directory'})
+        db.custom_footer = {"Not in a git directory"}
     end
 else
     get_dashboard_git_status()
