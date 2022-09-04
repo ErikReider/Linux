fe78ef361d8651acb2bcb037836a93f6aab9a67f
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-09-04 20:46:52 +0200

    NVIM: Theme tweaks

--- dotfiles/nvim/lua/theme.lua
+++ dotfiles/nvim/lua/theme.lua
@@ -3,14 +3,33 @@ vim.o.termguicolors = true
 -- Colors: https://vim.fandom.com/wiki/Xterm256_color_names_for_console_Vim
 -- Colors2: https://www.ditig.com/256-colors-cheat-sheet
 
--- Enable italic comment
-vim.g.vscode_italic_comment = 1
 -- Gets called when the GTK color-scheme changes
 require("custom.gsettings_watcher").init(function(style)
     vim.g.gtk_style = style
     vim.o.background = style
     vim.g.vscode_style = style
     vim.cmd([[colorscheme vscode]])
+
+    local c = require("vscode.colors")
+    local isDark = vim.o.background == 'dark'
+    local lspRef = {
+        bg = isDark and c.vscPopupHighlightGray or c.vscPopupHighlightLightBlue,
+        bold = true
+    }
+    require("vscode").setup({
+        transparent = false,
+        italic_comments = true,
+        disable_nvimtree_bg = false,
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+            -- this supports the same val table as vim.api.nvim_set_hl
+            -- use colors from this colorscheme by requiring vscode.colors!
+
+            LspReferenceText = lspRef,
+            LspReferenceRead = lspRef,
+            LspReferenceWrite = lspRef
+        }
+    })
 end)
 
 -- Highlights yanked region
@@ -18,7 +37,7 @@ vim.api.nvim_create_augroup("highlight_yank", {clear = true})
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = "highlight_yank",
     callback = function()
-        vim.highlight.on_yank({higroup = 'IncSearch', timeout = 1000})
+        vim.highlight.on_yank({higroup = "IncSearch", timeout = 1000})
     end
 })
 
@@ -33,21 +52,10 @@ vim.opt.listchars:append("extends:⟩")
 vim.opt.listchars:append("precedes:⟨")
 
 -- Word highlight
-vim.api.nvim_set_hl(0, "illuminate", {link = "LspReferenceText"})
-vim.api.nvim_set_hl(0, "illuminatedWord", {link = "LspReferenceText"})
-for _, v in
-    ipairs({"LspReferenceText", "LspReferenceRead", "LspReferenceWrite"}) do
-    vim.highlight.create(v, {cterm = "bold", gui = "bold"}, false)
-end
-
--- Underlines
-vim.highlight.create("DiagnosticUnderlineError",
-                     {gui = "undercurl", cterm = "undercurl"})
-vim.highlight.create("DiagnosticUnderlineWarn",
-                     {gui = "undercurl", cterm = "undercurl"})
-vim.highlight.create("DiagnosticUnderlineInfo",
-                     {gui = "undercurl", cterm = "undercurl"})
-
--- Nvim-tree fixes
-vim.api.nvim_set_hl(0, "NvimTreeGitIgnored", {link = "NvimTreeEmptyFolderName"})
-vim.highlight.create("NvimTreeGitNew", {guifg = "#73c991"})
+vim.api.nvim_set_hl(0, "IlluminatedWordText", {link = "LspReferenceText"})
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", {link = "LspReferenceRead"})
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", {link = "LspReferenceWrite"})
+-- for _, v in
+-- ipairs({"LspReferenceText", "LspReferenceRead", "LspReferenceWrite"}) do
+-- vim.highlight.create(v, {cterm = "bold", gui = "bold"}, false)
+-- end
