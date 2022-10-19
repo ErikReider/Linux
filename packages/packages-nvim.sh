7ec5f00f8b2a9f6011b50b0fc0d5f7762242bf8d
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-10-19 16:54:54 +0200

    Initscript: Added Lua and luarocks

--- packages/packages-nvim.sh
+++ packages/packages-nvim.sh
@@ -3,11 +3,14 @@
 export common=(
 "scdoc"
 "uncrustify"
+"luarocks"
 )
 export fedora=(
+"lua-devel"
 "texlive-scheme-basic"
 )
 export arch=(
+"lua"
 "texlive-bibtexextra"
 "texlive-gantt"
 "texlive-pictures"
