d1c01ecae80b69ffe41363bd8c1556785111c769
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-10-24 00:44:46 +0200

    Lure: Added swaylock-effects (my fork)

---
+++ dotfiles/swayConfig/lure_packages/swaylock-effects-erikreider.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+# vim: ft=sh
+_pkgname=swaylock-effects
+name="$_pkgname-erikreider-git"
+_ver="1.6.10"
+version="$_ver-r241.b3a375a"
+release=1
+desc="A fancier screen locker for Wayland."
+homepage="https://github.com/ErikReider/swaylock-effects"
+architectures=("amd64")
+license=("MIT")
+provides=("swaylock=$_ver" "swaylock-effects=$_ver")
+conflicts=("swaylock")
+
+build_deps=("git" "meson" "scdoc" "wayland-protocols")
+build_deps_fedora=("git" "meson" "scdoc" "wayland-protocols-devel")
+
+deps=("libxkbcommon" "cairo" "gdk-pixbuf2" "pam")
+deps_fedora=("libxkbcommon-devel" "cairo-devel" "gdk-pixbuf2-devel" "pam-devel")
+
+sources=("git+https://github.com/ErikReider/swaylock-effects.git")
+checksums=("SKIP")
+
+version() {
+    cd "$srcdir/$_pkgname"
+    printf "$_ver-r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+}
+
+build() {
+    cd "$srcdir/$_pkgname"
+    meson build --prefix=/usr
+    ninja -C build
+}
+
+package() {
+    cd "$srcdir/$_pkgname"
+    DESTDIR="$pkgdir" ninja -C build install
+}
