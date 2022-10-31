b363b0c2e7dc9fa91bac13b6096650e9b8eb899b
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2022-10-31 18:44:27 +0100

    Sway: Updated waybar headetcontrol script

--- dotfiles/swayConfig/waybar/scripts/headsetcontrol.sh
+++ dotfiles/swayConfig/waybar/scripts/headsetcontrol.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-charge=`headsetcontrol -b 2>&1 | grep "Battery: " | awk '{ print $2 }'`
-if [ "$charge" != "Unavailable" ]; then
-    if [[ "$charge" == "" ]]; then charge="100%"; fi
-    if [[ "$charge" == "Charging" ]]; then 
+
+# Get headsetcontrol result and exit if invalid
+headetResult=$(headsetcontrol -b 2>&1) || {
+    echo ""
+    exit 1
+}
+
+if [[ $headetResult != *"Unavailable"* ]]; then
+    charge=$(echo $headetResult | awk '{print $NF}')
+    if [[ $charge == "" ]]; then
+        charge="100%"
+    elif [[ $charge == "Charging" ]]; then
         charge=""
     fi
-    echo "  $charge"
+    echo " $charge"
 else
     echo
 fi
