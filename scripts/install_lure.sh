6d37b6a322c2f7e3a51efbf5f09e056f603237ef
Erik Reider <erik.reider@protonmail.com>
2023-03-01 17:36:00 +0100

    Init: Updated install_lure.sh script

--- scripts/install_lure.sh
+++ scripts/install_lure.sh
@@ -1,23 +1,90 @@
 #!/bin/bash
 
-if command -v lure &>/dev/null; then
-    lure up
-    exit 0
-fi
+info() {
+    echo $'\x1b[32m[INFO]\x1b[0m' "$@"
+}
 
-lure_url=$(curl -s https://gitea.arsenm.dev/api/v1/repos/Arsen6331/lure/releases | jq -r '.[0]["assets"] | map(select(.name | contains("'"$(uname -m)"'"))) | map( { (.name|tostring|split(".")[-1]): . } ) | add | .["rpm"]["browser_download_url"]')
-sha_url=$(curl -s https://gitea.arsenm.dev/api/v1/repos/Arsen6331/lure/releases | jq -r '.[0]["assets"] | map(select(.name | contains("checksums.txt"))) | add | .["browser_download_url"]')
+warn() {
+    echo $'\x1b[31m[WARN]\x1b[0m' "$@"
+}
 
-mkdir /tmp/lure
-curl -fLo "/tmp/lure/lure.rpm" $lure_url
-curl -fLo "/tmp/lure/checksums.txt" $sha_url
-
-sha=$(sha256sum /tmp/lure/lure.rpm | awk '{print $1}')
-cat /tmp/lure/checksums.txt | grep "$sha" &> /dev/null || {
-    echo CHECKSUMS NOT MATCHING!
+error() {
+    echo $'\x1b[31;1m[ERR]\x1b[0m' "$@"
     exit 1
 }
 
-sudo dnf install /tmp/lure/lure.rpm
+installPkg() {
+    rootCmd=""
+    if command -v doas &>/dev/null; then
+        rootCmd="doas"
+    elif command -v sudo &>/dev/null; then
+        rootCmd="sudo"
+    else
+        warn "No privilege elevation command (e.g. sudo, doas) detected"
+    fi
 
-echo "Done!"
+    case $1 in
+        pacman) $rootCmd pacman -U "${@:2}" ;;
+        apk) $rootCmd apk add --allow-untrusted "${@:2}" ;;
+        *) $rootCmd "$1" install "${@:2}" ;;
+    esac
+}
+
+if command -v lure &>/dev/null; then
+    info "Lure is already installed. Quitting..."
+    exit 0
+fi
+
+if ! command -v curl &>/dev/null; then
+    error "This script requires the curl command. Please install it and run again."
+fi
+
+pkgFormat=""
+pkgMgr=""
+if command -v pacman &>/dev/null; then
+    info "Detected pacman"
+    pkgFormat="pkg.tar.zst"
+    pkgMgr="pacman"
+elif command -v dnf &>/dev/null; then
+    info "Detected dnf"
+    pkgFormat="rpm"
+    pkgMgr="dnf"
+else
+    error "No supported package manager detected!"
+fi
+
+latestVersion=$(curl -sI 'https://gitea.arsenm.dev/Arsen6331/lure/releases/latest' | grep -o 'location: .*' | rev | cut -d '/' -f1 | rev | tr -d '[:space:]')
+info "Found latest LURE version:" "$latestVersion"
+
+[ -d "/tmp/lure" ] || mkdir /tmp/lure
+fname="/tmp/lure/lure-${latestVersion}.${pkgFormat}"
+sum_fname="/tmp/lure/lure-${latestVersion}-checksums.txt"
+
+if ! [ -f "$fname" ] || ! [ -f "$sum_fname" ]; then
+    sum_url="https://gitea.arsenm.dev/Arsen6331/lure/releases/download/${latestVersion}/checksums.txt"
+    info "Downloading LURE package"
+    curl -L "$sum_url" -o "$sum_fname"
+
+    url="https://gitea.arsenm.dev/Arsen6331/lure/releases/download/${latestVersion}/linux-user-repository-${latestVersion#v}-linux-$(uname -m).${pkgFormat}"
+    info "Downloading LURE package"
+    curl -L "$url" -o "$fname"
+fi
+
+# Validate
+info "Validating Checksums..."
+sha=$(sha256sum "$fname" | awk '{print $1}')
+grep "$sha" "$sum_fname" &>/dev/null || {
+    error "CHECKSUMS NOT MATCHING!"
+    rm "$fname"
+    rm "$sum_fname"
+    exit 1
+}
+
+info "Installing LURE package"
+installPkg "$pkgMgr" "$fname"
+
+info "Cleaning up"
+rm "$fname"
+rm "$sum_fname"
+
+info "Done!"
