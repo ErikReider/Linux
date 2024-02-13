912823a607f63d9b3fe720e85e219dbe4035a0df
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2024-02-13 22:35:12 +0100

    NVIM: Updated Neovide settings

--- dotfiles/nvim/lua/neovide.lua
+++ dotfiles/nvim/lua/neovide.lua
@@ -1,13 +1,31 @@
-if vim.g.neovide == true then
+if vim.g.neovide then
     vim.g.neovide_profiler = false
+    vim.g.neovide_confirm_quit = true
+
     vim.g.neovide_cursor_animation_length = 0
     vim.g.neovide_scroll_animation_length = 0.3
     vim.g.neovide_cursor_vfx_mode = ""
+
     vim.g.neovide_refresh_rate = 60
+    vim.g.neovide_refresh_rate_idle = 5
+
+    vim.g.transparency = 0.8
     vim.g.neovide_transparency = 0.8
-    vim.g.neovide_floating_blur_amount_x = 0.0
-    vim.g.neovide_floating_blur_amount_y = 0.0
-    vim.cmd('set guifont="FiraCode Nerd font"')
+
+    -- Floating blur
+    vim.g.neovide_floating_blur_amount_x = 8.0
+    vim.g.neovide_floating_blur_amount_y = 8.0
+
+    -- Floating Shadow
+    vim.g.neovide_floating_shadow = true
+    vim.g.neovide_floating_z_height = 10
+    vim.g.neovide_light_angle_degrees = 90
+    vim.g.neovide_light_radius = 10
+
+    -- Scroll Animation Length
+    vim.g.neovide_scroll_animation_length = 0.2
+
+    vim.o.guifont = "FiraCode Nerd font"
 
     ---System clipboard
     -- Copy
