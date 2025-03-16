a6d2ae935aaa2f053afe6ea05ac5a0791b303494
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2025-03-16 12:08:30 +0100

    NVIM: Made lua LSP start faster

--- dotfiles/nvim/lua/plugins/lsp/servers.lua
+++ dotfiles/nvim/lua/plugins/lsp/servers.lua
@@ -50,9 +50,15 @@ nvim_lsp.lua_ls.setup({
                 globals = { "vim" }
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    -- Depending on the usage, you might want to add additional paths here.
+                    -- "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                -- library = vim.api.nvim_get_runtime_file("", true)
             },
             telemetry = { enable = false },
             format = { enable = false }
