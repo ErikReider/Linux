eaddf2e638b4db7861bfce4db1a640dcd09c0d81
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2025-09-25 20:15:21 +0200

    Sway: Switch to my own sway utilities

--- dotfiles/.config/sway/scripts/lock.sh
+++ dotfiles/.config/sway/scripts/lock.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+SCRIPT_PATH="$(realpath "$0")"
+
 lock() {
     # profilePic=$(python3 ~/.config/sway/scripts/profilePicture.py)
     # swaylock --daemonize --indicator-image "$profilePic"
-    hyprlock 2>/dev/null 1>&2 &
-    disown
+
+    # hyprlock 2>/dev/null 1>&2 &
+    # disown
+
+    swaysettings-locker --daemonize
+    sleep 1
 }
 
 start() {
@@ -14,13 +20,14 @@ start() {
     # Wait until the processes have been shut down
     while pgrep -u $UID -x swayidle >/dev/null; do sleep 1; done
 
-    # Locks after 10min, suspends after 10min (20min). Lock screen before sleep.
-    # If already locked, suspend after 10 mins instead of 20 min
+    # Idles after 8min, locks after 10min (2min post idle), suspends after 10min (20min).
+    # Lock screen before sleep. If already locked, suspend after 10 mins instead of 20 min
     swayidle -w \
-        timeout 600 "(pidof hyprlock && systemctl suspend) || loginctl lock-session" \
+        idlehint 480 \
+        timeout 600 "(pidof swaysettings-locker && systemctl suspend) || loginctl lock-session" \
         timeout 1200 "systemctl suspend" \
-        before-sleep "loginctl lock-session" \
-        lock "$HOME/.config/sway/scripts/lock.sh --lock" 1>&2 &
+        before-sleep "$SCRIPT_PATH --lock" \
+        lock "$SCRIPT_PATH --lock" 1>&2 &
     disown
 }
 
